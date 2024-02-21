41074e91c7b3c5e61136b055f6793e8e8d956d89
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2024-02-21 18:51:10 +0100

    Adding example test

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,15 +1,21 @@
 const app = require('../src/app');
 const database = require('../src/database');
+const supertest = require('supertest');
 
 describe('Tests', () => {
 
     beforeAll(async () => {
         // INIT
-        await database.authentificate();
+        await database.authenticate();
         await database.sync({ force: true });
     });
 
+    it('Example should work', async () => {
+        const helloWorld = await supertest(app).get('/');
 
+        expect(helloWorld.statusCode).toBe(200);
+        expect(helloWorld.text).toBe('Hello world');
+    });
 
     afterAll(async () => {
         // CLEANUP
