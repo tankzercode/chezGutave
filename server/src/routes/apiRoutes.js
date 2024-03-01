376e75760a7c94684b9dbcc3d27667d72a801f0e
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-03-01 11:27:41 +0100

    up

--- server/src/routes/apiRoutes.js
+++ server/src/routes/apiRoutes.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Importation des contrôleurs
-const userController = require('../controllers/userController');
-const logementController = require('../controllers/logementController');
-const equipementController = require('../controllers/equipementController');
-const ratingController = require('../controllers/ratingController');
-const reservationController = require('../controllers/reservationController');
+const userController = require("../controllers/userController");
+const logementController = require("../controllers/logementController");
+const equipementController = require("../controllers/equipementController");
+const ratingController = require("../controllers/ratingController");
+const reservationController = require("../controllers/reservationController");
 
 // Routes pour les utilisateurs
-router.post('/users', userController.createUser);
-router.get('/users', userController.getAllUsers);
+router.post("/createUser", userController.createUser);
+router.get("/getAllUsers", userController.getAllUsers);
 
 // Routes pour les logements
-router.post('/logements', logementController.createLogement);
-router.get('/logements', logementController.getAllLogements);
+router.post("/createLogement", logementController.createLogement);
+router.get("/getAllLogements", logementController.getAllLogements);
 
 // Routes pour les équipements
-router.post('/equipements', equipementController.createEquipement);
-router.get('/equipements', equipementController.getAllEquipements);
+router.post("/createEquipement", equipementController.createEquipement);
+router.get("/getAllEquipements", equipementController.getAllEquipements);
 
 // Routes pour les évaluations
-router.post('/ratings', ratingController.createRating);
-router.get('/ratings', ratingController.getAllRatings);
+router.post("/createRating", ratingController.createRating);
+router.get("/getAllRatings", ratingController.getAllRatings);
 
 // Routes pour les réservations
-router.post('/reservations', reservationController.createReservation);
-router.get('/reservations', reservationController.getAllReservations);
-router.put('/reservations/:reservationId', reservationController.updateReservation);
-router.delete('/reservations/:reservationId', reservationController.deleteReservation);
+router.post("/createReservation", reservationController.createReservation);
+router.get("/getAllReservations", reservationController.getAllReservations);
+router.put(
+  "/updateReservation/:reservationId",
+  reservationController.updateReservation
+);
+router.delete(
+  "/deleteReservation/:reservationId",
+  reservationController.deleteReservation
+);
 
 module.exports = router;
