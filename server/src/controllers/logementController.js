1243910f4768b3e5215464866f7e862969abb26b
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-03-11 09:51:30 +0100

    up

--- server/src/controllers/logementController.js
+++ server/src/controllers/logementController.js
@@ -1,34 +1,62 @@
-const db = require('../models/index');
+const db = require("../models/index");
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
 
-// Créer un nouveau logement
+// Fonction pour sauvegarder l'image et obtenir le hash
+function saveImageAndGetHash(imageBuffer) {
+  const hash = crypto.createHash("sha256").update(imageBuffer).digest("hex");
+  const filename = `${hash}.png`;
+  const imagePath = path.join(__dirname, "..", "uploads", filename);
+
+  fs.writeFileSync(imagePath, imageBuffer);
+
+  return filename;
+}
+
+// Modifier la fonction createLogement pour sauvegarder les images avec le hash
 exports.createLogement = async (req, res) => {
-    try {
-        const { images, secteur, description, tarif_bas, tarif_moyen, tarif_haut, m_carre, chambre, salle_de_bain, categorie, type } = req.body;
-        const newLogement = await db.Logement.create({
-            images,
-            secteur,
-            description,
-            tarif_bas,
-            tarif_moyen,
-            tarif_haut,
-            m_carre,
-            chambre,
-            salle_de_bain,
-            categorie,
-            type
-        });
-        res.status(201).send(newLogement);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+  try {
+    // Supposer que les images sont envoyées sous forme de buffer dans une requête multipart/form-data
+    const {
+      secteur,
+      description,
+      tarif_bas,
+      tarif_moyen,
+      tarif_haut,
+      m_carre,
+      chambre,
+      salle_de_bain,
+      categorie,
+      type,
+    } = req.body;
+    const imageBuffers = req.files; // 'req.files' doit être fourni par un middleware de gestion de fichiers, tel que multer.
+
+    const imageFilenames = imageBuffers.map((buffer) =>
+      saveImageAndGetHash(buffer)
+    );
+
+    const newLogement = await db.Logement.create({
+      images: imageFilenames, // Stockez les noms des fichiers au lieu des buffers d'image
+      secteur,
+      description,
+      tarif_bas,
+      tarif_moyen,
+      tarif_haut,
+      m_carre,
+      chambre,
+      salle_de_bain,
+      categorie,
+      type,
+    });
+
+    res.status(201).send(newLogement);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
-// Récupérer tous les logements
+// Le reste du code reste inchangé
 exports.getAllLogements = async (req, res) => {
-    try {
-        const logements = await db.Logement.findAll();
-        res.status(200).send(logements);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+  // ... code existant ...
 };
